5e9356247dfd52813d91476229ff185e87b71623
Kiran_Presolv360 <kirannikam211@gmail.com>
2024-06-13 18:14:27 +0530

    All notice

--- src/test/java/Incase360/All_notice.java
+++ src/test/java/Incase360/All_notice.java
@@ -10,6 +10,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+
 @Test
 public class All_notice {
     public void Addnewnotice() throws InterruptedException {
@@ -19,7 +21,7 @@ public class All_notice {
         ChromeDriver driver = new ChromeDriver(options);
         driver.get("https://test.incase360.com/login");
         driver.manage().window().maximize();
-        Thread.sleep(5000);
+        Thread.sleep(3000);
         driver.findElement(By.name("userName")).sendKeys("admin@incase360.com");
         driver.findElement(By.name("userPassword")).sendKeys("1WbFG0Z84@");
         Thread.sleep(3000);
@@ -30,14 +32,14 @@ public class All_notice {
         Thread.sleep(3000);
         driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/section/main/div/div/div/div/div/div/div/form/div[4]/div[2]/button")).click();
         Thread.sleep(3000);
-//        driver.findElement(By.xpath("//*[@id=\"side-menu\"]/li[4]/a")).click();
-//        Thread.sleep(3000);
-//        driver.findElement(By.xpath("//*[@id=\"side-menu\"]/li[4]/ul/li[1]/a")).click();
-//        Thread.sleep(4000);
-//        driver.findElement(By.xpath("//*[@id=\"side-menu\"]/li[4]/a/span")).click();
-        driver.findElement(By.xpath("//*[@id=\"side-menu\"]/li[3]/a")).click();
-        Thread.sleep(4000);
-        driver.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div[2]/div/div/div[2]/button")).click();
+        driver.findElement(By.cssSelector("a[href='/admin/noticelist']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//*[@id=\"side-menu\"]/li[3]/ul/li[1]/a")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("button[class='btn btn-outline-primary']")).click();
+        //*[@id="layout-wrapper"]/div[2]/div/div/div[2]/div[1]/div/div/input
+        //*[@id="layout-wrapper"]/div[2]/div/div/div[2]/div[1]/div/div/button
+        //*[@id="layout-wrapper"]/div[2]/div/div/div[2]/button
         Thread.sleep(4000);
         driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[2]/div/div/div[2]/div[1]/div/div/input")).sendKeys("Font_001");
         Thread.sleep(4000);
@@ -47,8 +49,16 @@ public class All_notice {
         element.click();
         Thread.sleep(3000);
         driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[2]/div/div/div[2]/div[1]/div[2]/div[1]/div[1]/div/div[6]/div/div/button")).click();
-
-
+        ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
+        Thread.sleep(2000);
+        driver.switchTo().window(tabs.get(0));
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector(".swal2-actions > .swal2-confirm")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("body > div:nth-child(1) > div:nth-child(2) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(5) > div:nth-child(1) > div:nth-child(1) > a:nth-child(1) > button:nth-child(1)")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("div[class='main-content'] li:nth-child(2) a:nth-child(1)")).click();
+        driver.quit();
 
 
 
---
+++ src/test/java/Incase360/Exsiting_notice.java
@@ -0,0 +1,54 @@
+package Incase360;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+@Test
+public class Exsiting_notice {
+    public void Existing_notice() throws InterruptedException {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver(options);
+        driver.get("https://test.incase360.com/login");
+        driver.manage().window().maximize();
+        Thread.sleep(3000);
+        driver.findElement(By.name("userName")).sendKeys("admin@incase360.com");
+        driver.findElement(By.name("userPassword")).sendKeys("1WbFG0Z84@");
+        Thread.sleep(3000);
+        WebElement e = driver.findElement(By.id("captchaanswer"));
+        System.out.println("This is the value" + e.getAttribute("innerHTML"));
+        Thread.sleep(1000);
+        driver.findElement(By.id("captcha")).sendKeys(e.getAttribute("innerHTML"));
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/section/main/div/div/div/div/div/div/div/form/div[4]/div[2]/button")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//*[@id=\"side-menu\"]/li[5]/a")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div[2]/div/div/div[2]/div[2]/div[2]/div[1]/div[1]/div/div[6]/div/div/button")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("svg[aria-label='close']")).click();
+
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div[2]/div/div/div[2]/div[2]/div[2]/div[1]/div[1]/div/div[7]/div/div/button")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("svg[aria-label='close']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("body > div:nth-child(1) > div:nth-child(2) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(8) > div:nth-child(1) > div:nth-child(1) > a:nth-child(1) > button:nth-child(1)")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div[2]/div/div/div[1]/div/div/ol/li[2]/a")).click();
+        driver.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div[2]/div/div/div[2]/div[1]/div/div/input")).sendKeys("test2121");
+        driver.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div[2]/div/div/div[2]/div[1]/div/div/button")).click();
+        Thread.sleep(1000);
+      
+//        driver.findElement(By.xpath("/html/body/div[3]/div/div[6]/button[1]")).click();
+
+//        test2121
+
+
+    }
+}
